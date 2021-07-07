84c6a3159e1f56085522155b0f7cf8d12a615d5b
alexey.kasatikov <alexey.kasatikov@cxense.com>
2021-07-07 16:41:02 +0400

    test

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/alexkasatikov/oncallstats/internal/app/server"
 )
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/alertmanager", server.alertmanagerHandler)
-	mux.HandleFunc("/opsgenie", server.opsgenieHandler)
+	mux.HandleFunc("/alertmanager", server.AlertmanagerHandler)
+	mux.HandleFunc("/opsgenie", server.OpsgenieHandler)
 	log.Println("Started api server")
 
 	err := http.ListenAndServe(":4000", mux)
